95acb8ca13166120b2ebaf3acb7a0c3a39f27179
luoxue <luoxue@zhuanzhuan.com>
2019-06-18 14:59:35 +0800

    解决ios12.3.1版本bug

--- src/libs/utils.js
+++ src/libs/utils.js
@@ -11,3 +11,31 @@
 export const regTest = ({reg, str}) => {
     return reg.test(str);
 };
+
+export const IOSVersion = () => {
+  let str = navigator.userAgent.toLowerCase(); 
+  let ver = str.match(/cpu iphone os (.*?) like mac os/); 
+  try {
+      if(ver) ver = ver[1].replace(/_/g,".")
+  } catch (error) {
+      console.log(error)
+  }
+  return ver
+}
+
+export const compareVersion = (curV,reqV) => {
+  if(curV && reqV){
+     let arr1 = curV.split('.'),
+         arr2 = reqV.split('.');
+     let minLength=Math.min(arr1.length,arr2.length),
+         position=0,
+         diff=0;
+     while (position < minLength && ((diff = parseInt(arr1[position]) - parseInt(arr2[position]))==0)) {
+         position ++;
+     }
+     diff=(diff!=0)?diff:(arr1.length-arr2.length);
+     return diff>0;
+  }else{
+     return false;
+  }
+}
