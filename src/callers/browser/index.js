87c0d58f31eb9082ace94b4643d5a4f9cb3c8d5e
luoxue <luoxue@zhuanzhuan.com>
2019-06-18 14:57:52 +0800

    fix

--- src/callers/browser/index.js
+++ src/callers/browser/index.js
@@ -13,11 +13,25 @@ export default class BrowserCaller extends BaseCaller {
         location.href = options.__SCHEMA_PATH;
     }
     __tryLaunch (options) {
+        const iosVer = IOSVersion()
+        if (compareVersion(iosVer,'12.3.0')) options.delay = 2000
+
         this.__openApp(options);
-        setTimeout(() => {
+        const timer = setTimeout(() => {
             this.__download(options);
         }, options.delay);
+
+        const visibilitychange = function () {
+            const tag = document.hidden || document.webkitHidden;
+            tag && clearTimeout(timer)
+        }
+        document.addEventListener('visibilitychange', visibilitychange, false)
+        document.addEventListener('webkitvisibilitychange', visibilitychange, false)
+        window.addEventListener('pagehide', function () {
+            clearTimeout(timer)
+        }, false)
     }
+
     launch (opts) {
         const options = super.adaptOptions(opts);
         this.__tryLaunch(options)
