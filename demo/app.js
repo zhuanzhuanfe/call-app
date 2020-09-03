cb52f38204d32cb029ed22ce2a83995e9194b89f
huangjiaxing <huangjiaxing@zhuanzhuan.com>
2020-09-03 18:45:21 +0800

    feat: 增加不同app的通用链接跳转

--- demo/app.js
+++ demo/app.js
@@ -9,15 +9,23 @@ import CallApp from '../src'
 //   console.log('fail', e.detail)
 // })
 
+// setTimeout(() => {
+//   location.href = 'https://mjump.zhuanzhuan.com/zhuanzhuan/index.html';
+// }, 2000);
+
+// alert(0)
+
+// location.href = 'https://3rqhu6.openinstall.io/ulink/c/eyJtIjoieUdMdWFteEdsVFVBQUFGMFUzMGZUTWZDb1NVS0NFVnN1bTdTX0pZTUU3ai1sY3dMUEhDb2FWOTBQclFrYlp0cGJrYnUifQ==';
+
 const btn2 = document.getElementById('btn')
 const callApp = (window.callApp = new CallApp())
 btn2.addEventListener('click', function () {
   callApp.start({
-    path: 'jump/group/manage/jump', // 兼容app所有统跳地址
+    path: 'jump/core/myBuyList/jump', // 兼容app所有统跳地址
     channelId: '777',
     targetApp: 'zz',
     // wechatStyle: 1, // 1表示浮层右上角，2表示浮层按钮
-    // universal: true,
+    universal: true,
     // download: true,
   })
 })
--- src/callers/browser/universal.js
+++ src/callers/browser/universal.js
@@ -1,4 +1,11 @@
+const targetToPath = {
+  zz: 'zhuanzhuan',
+  zzseller: 'seller',
+  check: 'check',
+  yige: 'yige',
+}
+
 export default function (opt) {
   const { __SCHEMA_PATH } = opt
-  location.href = `https://mjump.zhuanzhuan.com/zhuanzhuan/index.html?path=${__SCHEMA_PATH}`
+  location.href = `https://mjump.zhuanzhuan.com/${targetToPath[opt.targetApp]}/index.html?path=${__SCHEMA_PATH}`
 }
