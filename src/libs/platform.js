0cd1d88ae5ef64ae44c626d920183e2b0eb31c39
zhironghao <zhironghao@zhuanzhuan.com>
2021-07-09 10:32:40 +0800

    feat: update gitignore

--- src/libs/platform.js
+++ src/libs/platform.js
@@ -4,20 +4,5 @@ export class Platform {
   constructor(opts = {}) {
     this.ua = opts.ua || navigator.userAgent
   }
-  getCurrentPlatform() {
-    const ua = this.ua.toLowerCase()
-    const defaultType = { name: 'browser' }
-    const plat = Object.assign(
-      {},
-      defaultType,
-      platformTypes.filter(plat => {
-        if (plat.reg.test(ua)) {
-          plat.reg.lastIndex = 0;
-          return true
-        }
-        return false
-      })[0]
-    )
-    return plat.name
-  }
+
 }
